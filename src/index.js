d0fc280b7b46fdc96ce2b59b241276237dc0123f
tecmoc <kidacero@hotmail.com>
2023-05-04 18:58:21 -0600

    html plugin, css injected

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './styles/main.scss';
 import generateJoke from './joke';
 
 console.log("Hola Weypack");
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -9,5 +10,23 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
   },
-
+  module: {
+    rules: [
+      {
+        test:/\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weypack',
+      filename: 'index.html',
+      template: 'src/template.html'
+    })
+  ]
 };
