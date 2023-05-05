53b1184b5616845795c9e2431dba9562d02735bd
tecmoc <kidacero@hotmail.com>
2023-05-05 12:06:20 -0600

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name][contenthash].js",
-    clean: true
+    clean: true,
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   devServer: {
     static: {
       directory: path.join(__dirname, "dist"),
@@ -20,13 +20,25 @@ module.exports = {
     port: 9000,
     open: true,
     historyApiFallback: true,
-    hot: true
+    hot: true,
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node-modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env'
+            ]
+          }
+        }
       },
     ],
   },
